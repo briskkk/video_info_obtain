5ce4a110a90e5e4f4c8b6a5a00a7b7ee634ea266
tianaleteng <18120126@bjtu.edu.cn>
2018-11-20 22:48:17 +0800

    加入了循环运行脚本的功能。

--- get_zone_video.py
+++ get_zone_video.py
@@ -9,11 +9,14 @@ import requests
 import platform
 from time import *
 from multiprocessing.dummy import Pool as ThreadPool
+from apscheduler.schedulers.background import BackgroundScheduler
 from classes import *
 import pymysql
 from secret import *
 
 video_info_all = []
+scheduler = BackgroundScheduler()
+URL_zone = "https://api.bilibili.com/x/web-interface/newlist?rid=17&type=0"
 
 # 先访问page1，然后可以获取到一共有多少个视频，按照每页放50个（经测试最多可以放50个，数量继续增加无效），做除法得到共分为多少页，再对每页进行遍历，获取每个视频的aid好以及标签，之后根据aid的URL获取每个aid的具体点赞量等信息。
 # get_page_num -- 获取视频排行总页数（每页设置为50个）
@@ -30,6 +33,7 @@ def get_page_num(URL_zone,page=1):
     
 # get_video_info -- 获取某个页码上的视频对象信息，返回每页的视频对象列表
 def get_video_info(page_num):
+    global URL_zone
     video_info_list = []
     URL_PAGE = URL_zone + "&pn={0}&ps=50".format(page_num)
     VID_INFO = requests.get(url = URL_PAGE).json()  # 从第page_count页获取视频列表
@@ -56,11 +60,29 @@ def get_video_info(page_num):
     return video_info_list  # 返回视频对象列表
     
 # 已完成视频信息的读取以及多线程的实现，接下来需要把信息存到数据库，周期运行代码功能。
-        
-def create_video_db():
+
+def create_tcount():
+    # 创建记录数据库表格操作次数的table
+    global cursor
+    cursor.execute("""create table if not exists t_count
+                    (opera_count int)
+                """)
+    cursor.execute("select opera_count from t_count")
+    if cursor.fetchone() == None:
+        cursor.execute("insert into t_count set opera_count={}".format(1))
+    conn.commit()
+
+def save_count():
+    global cursor,conn
+    cursor.execute("select opera_count from t_count")
+    operacount = cursor.fetchone()[0]
+    cursor.execute("update t_count set opera_count=%d" % (operacount+1))
+    return operacount
+
+def create_table_bili_video(TABLE_INDEX):
     # 创建数据库
     global cursor
-    cursor.execute("""create table if not exists bili_video
+    cursor.execute("""create table if not exists bili_video_{}
                     (v_aid int primary key,
                     v_title text,
                     v_biaoqian text,
@@ -75,48 +97,82 @@ def create_video_db():
                     v_like1 int,v_like2 int,v_like3 int,v_like4 int,v_like5 int,v_like6 int,v_like7 int,v_like8 int,
                     v_dislike1 int,v_dislike2 int,v_dislike3 int,v_dislike4 int,v_dislike5 int,v_dislike6 int,v_dislike7 int,v_dislike8 int
                     )
-                """)
+                """.format(TABLE_INDEX))
                 
-def save_video_db(video_instance_per_page):
-    # 将数据保存至本地
+def save_video_db(video_instance_per_page,table_index):
+    # 将数据保存至服务器MySQL数据库中
     global cursor,conn
-    sql = "insert into bili_video values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    for video_instance in video_instance_per_page:
-        row = [video_instance.aid,
-                video_instance.title,
-                video_instance.biaoqian,
-                video_instance.view,
-                video_instance.danmu,
-                video_instance.reply,
-                video_instance.shoucang,
-                video_instance.coin,
-                video_instance.share,
-                video_instance.like,
-                video_instance.dislike,
-                video_instance.author_mid,
-                video_instance.author_name
-                ]
-        try:
-            # 执行sql语句
-            cursor.execute(sql,row)
-        except:
-            # 发生错误时回滚
-            conn.rollback()
-    # 提交到数据库执行
-    conn.commit()
+    for video_instance in video_instance_per_page:  # 对于每一个video对象，首先查询是否已经在第table_index存在，如果不存在--insert，如果存在--update
+        for v_num in range(1,9):  # 查询第几行的v_view为空，将新值插入
+            cursor.execute("select v_view{0} from bili_video_{1} where v_aid={2}".format(v_num,table_index,video_instance.aid))
+            fet = cursor.fetchone()
+            print(fet)
+            if fet == None:  # 如果某个aid号还没有加到表中
+                sql_ins = "insert into bili_video_{0} set v_aid=%s,v_title=%s,\
+                v_biaoqian=%s,v_author_mid=%s,v_author_name=%s,v_view{1}=%s,\
+                v_danmu{2}=%s,v_reply{3}=%s,v_favor{4}=%s,v_coin{5}=%s,v_share{6}=%s,\
+                v_like{7}=%s,v_dislike{8}=%s".format(table_index,v_num,v_num,v_num,v_num,v_num,v_num,v_num,v_num)
+                row1 = [video_instance.aid,
+                        video_instance.title,
+                        video_instance.biaoqian,
+                        video_instance.view,
+                        video_instance.danmu,
+                        video_instance.reply,
+                        video_instance.shoucang,
+                        video_instance.coin,
+                        video_instance.share,
+                        video_instance.like,
+                        video_instance.dislike,
+                        video_instance.author_mid,
+                        video_instance.author_name
+                        ]
+                try:
+                    # 执行sql_ins语句
+                    cursor.execute(sql_ins,row1)
+                except:
+                    # 发生错误时回滚
+                    conn.rollback()
+                # 提交到数据库执行
+                conn.commit()
+                break
+            else:
+                data = fet[0]  # 返回某aid对应的view值，若为空，则跳出循环，并获得view序号
+                if not data:
+                    break
+        if v_num > 1 and data == None:  # 此aid已经有数据，更新值为NULL的值，即添加数据到view(num1)
+            sql_update = "update bili_video_{0} set v_view{1}=%s,v_danmu{2}=%s,\
+            v_reply{3}=%s,v_favor{4}=%s,v_coin{5}=%s,v_share{6}=%s,v_like{7}=%s,\
+            v_dislike{8}=%s".format(table_index,v_num,v_num,v_num,v_num,v_num,v_num,v_num,v_num)
+            row2 = [video_instance.view,
+                    video_instance.danmu,
+                    video_instance.reply,
+                    video_instance.shoucang,
+                    video_instance.coin,
+                    video_instance.share,
+                    video_instance.like,
+                    video_instance.dislike
+                    ]
+            try:
+                cursor.execute(sql_update,row2)
+            except:
+                conn.rollback()
+            conn.commit()
 
-
-if __name__ == "__main__":
-    
+def task_bili():
+    global cursor,conn,video_info_all,URL_zone
     # 将数据存入数据库
     # 建立和数据库系统的连接
     conn = pymysql.connect(host=HOST_IP,port=3306,user=USER,password=USER_PASSWD,database='video_db',charset='utf8')
     # 创建游标
     cursor = conn.cursor()
-    # 创建数据库中表格bili_video
-    create_video_db()
+    # 查表获取此次运行的序号（返回值为int，表示此次是第几次运行）
+    create_tcount()
+    call_count = save_count()
+    if call_count%8 == 1:  # 满8次创建一个新表
+        # 创建数据库中表格bili_video
+        table_index = call_count//8 + 1
+        create_table_bili_video(table_index)
     
-    URL_zone = "https://api.bilibili.com/x/web-interface/newlist?rid=17&type=0"
     num_of_pages = get_page_num(URL_zone)  # 获取视频排行总页数（每页设置为50个）
     print('num of pages: '+str(num_of_pages))
     print("#######################")
@@ -128,9 +184,14 @@ if __name__ == "__main__":
     video_info_all = video_info_all+ret
     pool.close()
     pool.join()
+    # 确定此次要在第几个表上进行存储操作
+    if call_count%8 != 0:
+        t_index = call_count//8 + 1
+    else:
+        t_index = call_count//8
     # 将所有视频信息传入数据库
     for video_per_page in video_info_all:
-        save_video_db(video_per_page)
+        save_video_db(video_per_page,t_index)
     # print(results[0]) # 每个视频的信息list
     # parse_video(results)
     endtime = time()
@@ -144,3 +205,18 @@ if __name__ == "__main__":
     cursor.close()
     # 关闭连接
     conn.close()
+
+# 循环执行
+def interval_trigger():
+    global scheduler
+    scheduler.add_job(func=task_bili, trigger='interval', seconds=30, id='interval_job1')  #返回一个apscheduler.job.Job的实例，可以用来改变或者移除job
+
+
+if __name__ == "__main__":
+    #scheduler.add_job(timedTask,'interval',hours=3)
+    interval_trigger()
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown() #关闭job
+    sleep(150)
